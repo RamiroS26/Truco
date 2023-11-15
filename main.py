d702106e860dc0a847478b45394f88ae2c5bf5f7
RamiroS26 <ramiro.silva826@gmail.com>
2023-11-15 16:07:44 -0300

    Initial integration to discord.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from game import Game
-from player import Player
+import settings
+import discord
+from discord.ext import commands
+from discord import app_commands
 
-def run_game():
-        print()
-        print("Ingresa el nombre del jugador 1")
-        p1 = input("-> ")
-        print("Ingresa el nombre del jugador 2")
-        p2 = input("-> ")
-        game = Game(p1, p2)
-        while not game.is_over():
-            print()
-            print("Inicia la mano.")
-            game.reset_hands()
-            game.start_hand()
-            game.table()
+
+
+logger = settings.logging.getLogger("bot")
+
+def run():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"{bot.user} connected to Discord.)")
+        bot.tree.copy_global_to(guild=settings.GUILDS_ID)
+        await bot.tree.sync(guild=settings.GUILDS_ID)
+
+    bot.run(settings.DISCORD_BOT_TOKEN, root_logger=True)
     
 if __name__ == "__main__":
-    run_game()
+    run()
 
