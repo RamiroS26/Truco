dce35ee19142296365a61ec5a5084ca296999a3b
RamiroS26 <ramiro.silva826@gmail.com>
2023-11-15 20:12:10 -0300

    Implement main command

--- cogs/truco_cog.py
+++ cogs/truco_cog.py
@@ -1,30 +1,66 @@
+from typing import Optional
 import discord
 from discord.ext import commands
+from truco import Game
 
 class TrucoCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-
+        self.games = {}
+    
     @commands.hybrid_command(name="truco", description="Empezar una partida de truco")
     async def truco(self, ctx: commands.Context, oponente: discord.Member):
         if oponente == ctx.author:
             await ctx.send("No podés empezar una partida con vos mismo.")
             return
         
-        await ctx.send(f"Te invitaron a una partida {oponente.mention}")
-        
-        button_aceptar = discord.ui.Button(label="✅ Aceptar", style=discord.ButtonStyle.success)
-        button_rechazar = discord.ui.Button(label="❌ Rechazar", style=discord.ButtonStyle.danger)
-        
-        view = discord.ui.View()
-        view.add_item(button_aceptar)
-        view.add_item(button_rechazar)
-        
-        embed = discord.Embed(title="Invitación a partida de truco", description="¿Aceptas la partida?")
-        
-        message = await ctx.send(embed=embed, view=view)
+        view = MainView(truco_cog=self, p1=ctx.author, oponente=oponente, timeout=120)
+        embed = discord.Embed(title="Invitación a partida de truco", description="¿Aceptas la partida?", colour=discord.Colour.green())
+        await ctx.send(content=f"{ctx.author.mention} te invitó a un truco, {oponente.mention}", embed=embed, view=view)
+        await view.wait()
 
+
+class MainView(discord.ui.View):
+
+    def __init__(self, oponente, p1, truco_cog, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.truco_cog = truco_cog
+        self.oponente = oponente
+        self.p1 = p1
+
+    @discord.ui.button(label="Aceptar", style=discord.ButtonStyle.success)
+    async def aceptar(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user == self.oponente:
+            await interaction.message.edit(content="Partida aceptada, el juego comienza.", view=None, embed=None)
+            print(self.p1.display_name, self.oponente.display_name)
+            truco_game = Game(self.p1, self.oponente)
+            if interaction.channel.id not in self.truco_cog.games:
+                self.truco_cog.games[interaction.channel.id] = []
+            self.truco_cog.games[interaction.channel.id].append(truco_game)
+            print(self.truco_cog.games)
+
+    @discord.ui.button(label="Rechazar", style=discord.ButtonStyle.danger)
+    async def rechazar(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user == self.oponente:
+            await interaction.message.edit(content="Partida rechazada.", view=None, embed=None)
+
+    """   def check(interaction):
+            return interaction.message.id == message.id
+
+        interaction, button = await self.bot.wait_for("button_click", check=check)
+        
+        if button == "aceptar":
+            await interaction.response.send_message("Partida aceptada, el juego comienza.")
+            # Iniciar un nuevo juego y agregarlo a la lista de juegos del canal
+            truco_game = Game()
+            if ctx.channel.id not in self.truco_cog.games:
+                self.truco_cog.games[ctx.channel.id] = []
+            self.truco_cog.games[ctx.channel.id].append(truco_game)
+            print(self.truco_cog.games)
+            #await truco_game.iniciar_juego(ctx.author, oponente)
+        elif button.custom_id == "rechazar":
+            await interaction.respond(content="Partida rechazada.") """
+   
 
 async def setup(bot):
     await bot.add_cog(TrucoCog(bot))
